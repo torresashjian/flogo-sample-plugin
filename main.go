bfc2dea75ed5ce1bd700c64aeddd912e63585771
Miguel Torres <migueltorreslopez@gmail.com>
2017-05-02 14:58:10 -0700

    Add plugin

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
-package flogo_sample_plugin
+package main
 
 
-import "fmt"
+import (
+	"fmt"
+	"plugin"
+	)
 
-func Serialize(){
-	fmt.Println("In Serialize method")
+func main(){
+	fmt.Println("In main method")
+	p, err := plugin.Open("./plugin.so")
+	if err != nil {
+		fmt.Println("Error on loading")
+		fmt.Println(err.Error())
+		panic(err.Error())
+	}
+	
+	f, err := p.Lookup("Serialize")
+	if err != nil {
+		fmt.Println("Error on opening")
+		fmt.Println(err.Error())
+		panic(err.Error())
+	}
+	
+	f.(func())()
+
 }
---
+++ plugin.go
@@ -0,0 +1,8 @@
+package main
+
+
+import "fmt"
+
+func Serialize(){
+	fmt.Println("In Serialize method in plugin")
+}
